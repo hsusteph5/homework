fefc41bf95416b4da5fb60e4549f40fe2082a69a
Stephanie Hsu <hsusteph5@gmail.com>
2018-08-23 23:16:38 -0700

    Add no repeats to set method in map

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -4,13 +4,14 @@ class Map
   end
 
   def set(key, value)
-    @array << [key, value]
+    @array << [key, value] if self.get(key).nil?
   end
 
   def get(key)
     @array.each do |arr|
       return arr if arr[0] == key
     end
+    nil
   end
 
   def delete(key)
