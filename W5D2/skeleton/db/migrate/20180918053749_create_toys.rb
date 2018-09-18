13ed7ad1a6ff42bb1c6710a8078d361937f6d2e6
Stephanie Hsu <hsusteph5@gmail.com>
2018-09-17 22:43:34 -0700

    Add CreateToys table

---
+++ W5D2/skeleton/db/migrate/20180918053749_create_toys.rb
@@ -0,0 +1,13 @@
+class CreateToys < ActiveRecord::Migration[5.1]
+  def change
+    create_table :toys do |t|
+      t.integer :user_id, null: false
+      t.string :name, null: false
+      t.integer :toyable_id, null: false
+      t.string :toyable_type, null: false
+    end
+    add_index :toys, :name, unique: true
+    add_index :toys, :toyable_id
+    add_index :toys, [:name, :toyable_id, :toyable_type], unique: true
+  end
+end
