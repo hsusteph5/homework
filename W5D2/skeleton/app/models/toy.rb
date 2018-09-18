f4fb367a579dd31bb838faba9ce1560614383755
Stephanie Hsu <hsusteph5@gmail.com>
2018-09-17 22:47:44 -0700

    Add part to Toy Model

--- W5D2/skeleton/app/models/toy.rb
+++ W5D2/skeleton/app/models/toy.rb
@@ -1,4 +1,6 @@
 class Toy < ApplicationRecord
   validates :name, uniqueness: { scope: [:toyable] }
   belongs_to :toyable, polymorphic: true
+    foreign_key: :toyable_id,
+    class_name: :Toy
 end
